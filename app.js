b5a393285b3578debef2793fbb7569a054b70b38
Blaze <sandevabeykoon123@gmail.com>
2020-08-08 12:24:20 +0530

    8.8.2020

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const http = require('http');
+const fs = require('fs')
+const port = 3000;
 
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data){
+
+    if (error) {
+        res.writeHead(404);
+        res.write('error: File Not Found')
+    }else{
+        res.write(data);
+    }
+    res.end()
+    })
+})
+
+server.listen(port, function(error){
+    if(error) {
+        console.log('Something went wrong ', error)
+        
+    }else{
+        console.log('server is listenting on port ' + port)
+    }
+
+})
--- script.js
+++ script.js
@@ -1,46 +1,52 @@
 var navObj = document.querySelector(".newdiv");
-var state = navObj.style.display = "none";
+var state = (navObj.style.display = "none");
 function onNav() {
     state = navObj.style.display = "grid";
 }
 
 function offNav() {
-    
-    state = navObj.style.display = "none";
+    state = navObj.style.display = "none"; }
+const srcOne = "http://198.178.123.8:8404/;";
+const srcTwo = "http://live.tnlrn.com:8010/live.mp3";
+const srcThree = "http://209.133.216.3:7048/;";
+const srcFour = "http://209.133.216.3:7058/;stream.mp3";
 
-}
+var audio = document.querySelector(".staticAudio");
 
-const srcOne = 'http://198.178.123.8:8404/;';
-const srcTwo = 'http://live.tnlrn.com:8010/live.mp3';
-const srcThree = 'http://209.133.216.3:7048/;';
-const srcFour = 'http://shaincast.caster.fm:47814/listen.mp3';
-
-var audio = document.querySelector('.staticAudio');
-
-
-document.getElementById('001btn').addEventListener('click', function(){
+document.getElementById("001btn").addEventListener("click", () => {
     audio.src = srcOne;
     console.log(audio.currentSrc);
-})
-document.getElementById('002btn').addEventListener('click', function(){
+});
+document.getElementById("002btn").addEventListener("click", () => {
     audio.src = srcTwo;
     console.log(audio.currentSrc);
-})
-document.getElementById('003btn').addEventListener('click', function(){
+});
+document.getElementById("003btn").addEventListener("click", () => {
     audio.src = srcThree;
     console.log(audio.currentSrc);
-})
-document.getElementById('004btn').addEventListener('click', function(){
+});
+document.getElementById("004btn").addEventListener("click", () => {
     audio.src = srcFour;
     console.log(audio.currentSrc);
-})
-
+});
 
 function audioPlay() {
     audio.play();
 }
 
-function pauseAudio(){
+function pauseAudio() {
     audio.pause();
 }
 console.dir(audio);
+
+var divEvent = document.getElementsByClassName("nav-btn");
+for (var i = 0; i < divEvent.length; i++) {
+    divEvent[i].addEventListener("click", () => {
+        state = navObj.style.display = "grid";
+    });
+}
+
+navObj.addEventListener("mouseleave", offNav);
+
+
+
