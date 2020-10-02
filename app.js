6fe1b80ca2cfe33c472cd3a8c6ab6ec88c908709
Blaze <sandevabeykoon123@gmail.com>
2020-10-02 08:53:17 +0530

    updated app.js for local host support.

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-const http = require('http');
-const fs = require('fs')
-const port = 3000;
+var express = require("express");
+var app = express();
+var path = require("path");
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, {'content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data){
+app.use(express.static(__dirname));
 
-    if (error) {
-        res.writeHead(404);
-        res.write('error: File Not Found')
-    }else{
-        res.write(data);
-    }
-    res.end()
-    })
-})
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-server.listen(port, function(error){
-    if(error) {
-        console.log('Something went wrong ', error)
-        
-    }else{
-        console.log('server is listenting on port ' + port)
-    }
-
-})
+app.listen(8080);
